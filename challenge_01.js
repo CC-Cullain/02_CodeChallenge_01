1a792d738c3a94a2e9a3f6e591b4665f6500413c
Cullain <lukas.gedrat@gmx.net>
2023-05-09 14:53:53 +0200

    If und Ifelse Abfragen

--- challenge_01.js
+++ challenge_01.js
@@ -16,6 +16,12 @@ firstZahl =  parseInt(firstZahl); // Umwandlung vom String in Integer Wert
 let secondZahl = prompt("Bitte zweite Zahl eingeben"); 
 secondZahl = parseInt(secondZahl); // Umwandlung vom String in Integer Wert
 
+// If Abfrage damit unterschiedliche Zahlen gewählt werden
+if (firstZahl == secondZahl) {
+   let secondZahl = prompt("Nimm ne andere Zahl")
+   secondZahl = parseInt(secondZahl);
+}
+
 /*
 Konvertierung in Integer ist wichtig damit es als Zahl erkannt
  und somit als Rechenvariable gesehen wird
@@ -24,3 +30,15 @@ Konvertierung in Integer ist wichtig damit es als Zahl erkannt
 let summe = firstZahl + secondZahl;
 
 console.log("Die Summe ist: " + summe);
+
+//Zusatzabfrage ob die Summe korrekt ist
+
+let richtig = prompt("Welche Summe haben sie errechenet?")
+richtig = parseInt(richtig);
+
+
+if (summe == richtig) {
+    console.log("Korrekt")
+} else {
+    console.log("Nö leider falsch")
+} 
