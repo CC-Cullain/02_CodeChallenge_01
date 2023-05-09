eda31d60dd44ff8389796f7164630fc62f514539
Cullain <lukas.gedrat@gmx.net>
2023-05-09 15:00:22 +0200

    Nette Formatierung

--- challenge_01.js
+++ challenge_01.js
@@ -11,21 +11,21 @@ Geben Sie die Summe der Zahlen in die Konsole aus:
 
 
 let firstZahl = prompt("Bitte erste Zahl eingeben");
-firstZahl =  parseInt(firstZahl); // Umwandlung vom String in Integer Wert
+    firstZahl = parseInt(firstZahl); // Umwandlung vom String in Integer Wert
 
 let secondZahl = prompt("Bitte zweite Zahl eingeben"); 
-secondZahl = parseInt(secondZahl); // Umwandlung vom String in Integer Wert
+    secondZahl = parseInt(secondZahl); // Umwandlung vom String in Integer Wert
+/*
+Konvertierung in Integer ist wichtig damit es als Zahl erkannt
+und somit als Rechenvariable gesehen wird
+*/
 
 // If Abfrage damit unterschiedliche Zahlen gewählt werden
 if (firstZahl == secondZahl) {
-   let secondZahl = prompt("Nimm ne andere Zahl")
-   secondZahl = parseInt(secondZahl);
+        let secondZahl = prompt("Nimm ne andere Zahl")
+            secondZahl = parseInt(secondZahl);
 }
 
-/*
-Konvertierung in Integer ist wichtig damit es als Zahl erkannt
- und somit als Rechenvariable gesehen wird
- */
 
 let summe = firstZahl + secondZahl;
 
@@ -34,11 +34,11 @@ console.log("Die Summe ist: " + summe);
 //Zusatzabfrage ob die Summe korrekt ist
 
 let richtig = prompt("Welche Summe haben sie errechenet?")
-richtig = parseInt(richtig);
+    richtig = parseInt(richtig);
 
 
 if (summe == richtig) {
     console.log("Korrekt")
 } else {
-    console.log("Nö leider falsch")
+        console.log("Nö leider falsch")
 } 
